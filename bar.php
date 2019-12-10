4a948ed3a36fadaaf8ed0d3cd221a63d3c035c78
Fynn Freyer <FynnFreyer@users.noreply.github.com>
2019-12-10 01:50:39 +0100

    Add files via upload

---
+++ bar.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Foo</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<a id="foo" href="index.php">Foo</a>
+		<a id="bar" href="bar.php">Bar</a>
+		<a id="baz" href="baz.php">Baz</a>
+	</body>
+</html>
