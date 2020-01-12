1f2c8f6eb554a7941d8d2c9f666d6edef85b4ec6
Fynn Freyer <fynn.freyer@googlemail.com>
2020-01-12 19:50:14 +0100

    added table

---
+++ table.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Table</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<a id="foo" href="index.php">Foo</a>
+        <table id="table">
+            <tbody> 
+                <tr> 
+                    <td>Zahlen</td>
+                </tr>
+                <tr> 
+                    <td>5</td>
+                </tr>
+                <tr> 
+                    <td>6</td>
+                </tr>
+                <tr> 
+                    <td>11</td>
+                </tr>
+            </tbody>
+        </table>
+	</body>
+</html>
